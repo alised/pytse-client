77836167a55c35c78a93f51b18d7afba02eeedcb
alised <seddighi.ali@gmail.com>
2025-02-20 15:11:10 +0330

    fix deprecated parameter in beautifulsoup.find()

--- tests/test_download/download_all.py
+++ tests/test_download/download_all.py
@@ -7,5 +7,5 @@ This process takes a long time so it's not part of the unit test
 from pytse_client import all_symbols, download, download_client_types_records
 
 if __name__ == "__main__":
-    download(all_symbols())
-    download_client_types_records(all_symbols())
+    download("وبملت")
+    download_client_types_records("وبملت")
